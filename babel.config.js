938d59392408c02295114585359cbb11828f46fe
daengae <dladmsxor434@naver.com>
2025-09-19 01:14:24 +0900

    chore: babel.config Update
    Ref: #

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,19 @@
-module.exports = function (api) {
-  api.cache(true)
+module.exports = function(api) {
+  api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
+      ['module:react-native-dotenv', {
+        "env": ["development", "production"],
+        "moduleName": "@env",
+        "path": ".env",
+        "blocklist": null,
+        "allowlist": null,
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }],
+      'react-native-worklets/plugin',
     ],
-  }
-}
+  };
+};
